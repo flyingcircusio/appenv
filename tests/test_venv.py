d5c099593853b052d3057ca3626dac68e84a09da
Christian Theune <ct@flyingcircus.io>
2020-04-25 20:29:58 +0200

    More test coverage for venv creation.

--- tests/test_venv.py
+++ tests/test_venv.py
@@ -7,3 +7,28 @@ def test_new_venv(tmpdir):
     assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'pip3'))
     assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'python'))
     assert os.path.exists(os.path.join(tmpdir, 'venv', 'lib'))
+
+    # doesn't break things
+    appenv.ensure_venv(os.path.join(tmpdir, 'venv'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'pip3'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'python'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'lib'))
+
+
+def test_new_broken_venv_recreated(tmpdir):
+    appenv.ensure_venv(os.path.join(tmpdir, 'venv'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'pip3'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'python'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'lib'))
+
+    os.unlink(os.path.join(tmpdir, 'venv', 'bin', 'pip3'))
+    with open(os.path.join(tmpdir, 'venv', 'asdf'), 'w'):
+        pass
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'asdf'))
+
+    # re-creates the venv
+    appenv.ensure_venv(os.path.join(tmpdir, 'venv'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'pip3'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'bin', 'python'))
+    assert os.path.exists(os.path.join(tmpdir, 'venv', 'lib'))
+    assert not os.path.exists(os.path.join(tmpdir, 'venv', 'asdf'))
