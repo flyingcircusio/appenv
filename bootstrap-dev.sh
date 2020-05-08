734ee6e7ec785b4871ddc28cdc7703c7c4a94492
Christian Theune <ct@flyingcircus.io>
2020-05-08 10:57:26 +0200

    add travis support

---
+++ bootstrap-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+
+PYTHONABS=${1:-python3}
+PYTHON=$(basename $PYTHONABS)
+
+rm -rf .Python bin lib include
+$PYTHONABS -m venv .
+bin/pip install --upgrade -r requirements.txt
