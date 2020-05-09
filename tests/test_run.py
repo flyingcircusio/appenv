b9a8ec21632156047211e32e22ce01ece568d9bd
Christian Theune <ct@flyingcircus.io>
2020-05-09 10:16:24 +0200

    Ensure we run the appenv script with the appropriate version of Python 'under test'.

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,6 +2,7 @@ import appenv
 import io
 import os.path
 import subprocess
+import sys
 
 
 def test_bootstrap_lockfile_existing_venv_broken_python():
@@ -21,6 +22,11 @@ def test_bootstrap_and_run_with_lockfile(meta_args, workdir, monkeypatch):
     appenv.init([], meta_args)
     appenv.update_lockfile([], meta_args)
     os.chdir(workdir / 'batou')
+    with open('batou', 'r') as f:
+        # Ensure we're called with the Python-interpreter-under-test.
+        script = '#!{}\n{}'.format(sys.executable, f.read())
+    with open('batou', 'w') as f:
+        f.write(script)
     output = subprocess.check_output('./batou --help', shell=True)
     assert output.startswith(b'usage: batou')
 
@@ -35,6 +41,11 @@ def test_bootstrap_and_run_python_with_lockfile(meta_args, workdir,
     appenv.init([], meta_args)
     appenv.update_lockfile([], meta_args)
     os.chdir(workdir / 'batou')
+    with open('batou', 'r') as f:
+        # Ensure we're called with the Python-interpreter-under-test.
+        script = '#!{}\n{}'.format(sys.executable, f.read())
+    with open('batou', 'w') as f:
+        f.write(script)
     output = subprocess.check_output(
         './batou appenv-python -c "print(1)"', shell=True)
     assert output == b'1\n'
