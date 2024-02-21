01bad4e9523c3903c10a4b826a2cb5e889a5eb0c
Eli Kogan-Wang <elikowa@gmail.com>
2024-02-21 12:30:11 +0100

    Add tests for prepare: creates venv symlink (appenv_dir / current)

--- tests/test_prepare.py
+++ tests/test_prepare.py
@@ -13,3 +13,16 @@ def test_prepare_creates_envdir(workdir, monkeypatch):
     env.update_lockfile()
     env.prepare()
     assert os.path.exists(env.appenv_dir)
+
+
+def test_prepare_creates_venv_symlink(workdir, monkeypatch):
+    # asserts that appenv_dir / "current" -> env_dir
+    monkeypatch.setattr('sys.stdin', io.StringIO('ducker\nducker<2.0.2\n\n'))
+    os.makedirs(os.path.join(workdir, 'ducker'))
+
+    env = appenv.AppEnv(os.path.join(workdir, 'ducker'))
+    env.init()
+    env.update_lockfile()
+    env.prepare()
+    assert os.path.islink(os.path.join(env.appenv_dir, "current"))
+    assert os.readlink(os.path.join(env.appenv_dir, "current")) == env.env_dir
