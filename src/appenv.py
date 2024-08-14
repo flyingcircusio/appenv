349c15d7f5f59463d9afa0bd1cdd17504b9307d6
Eli Kogan-Wang <elikowa@gmail.com>
2024-08-14 13:52:19 +0200

    upgrade core dependencies when creating venv

--- src/appenv.py
+++ src/appenv.py
@@ -87,7 +87,7 @@ def ensure_venv(target):
     version = sys.version.split()[0]
     python_maj_min = ".".join(str(x) for x in sys.version_info[:2])
     print("Creating venv ...")
-    venv.create(target, with_pip=False, symlinks=True)
+    venv.create(target, with_pip=False, symlinks=True, upgrade_deps=True)
 
     try:
         # This is trying to detect whether we're on a proper Python stdlib
